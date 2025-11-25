d66a288c96978ecc7df6a2cce1958acf5139dc05
sandaruk93 <sandaru.suranjaya@gmail.com>
2025-11-25 12:49:42 +0800

    Update Contact CTAs and enable Memory Reel scrolling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,23 @@
 import styles from './Contact.module.css';
+import SecondaryCtaHeroGlassy from './SecondaryCtaHeroGlassy';
 
 export default function Contact() {
     return (
         <section id="contact" className={styles.section}>
             <h2 className={styles.heading}>Get in Touch</h2>
             <div className={styles.links}>
-                <a href="mailto:sandaru.suranjaya@icloud.com" className={styles.link}>Email</a>
-                <a href="https://www.linkedin.com/in/sandaru-suranjaya/" target="_blank" rel="noopener noreferrer" className={styles.link}>LinkedIn</a>
-                <a href="https://www.instagram.com/sandaru.me/" target="_blank" rel="noopener noreferrer" className={styles.link}>Instagram</a>
-                <a href="https://x.com/sandaru_me" target="_blank" rel="noopener noreferrer" className={styles.link}>Twitter</a>
+                <SecondaryCtaHeroGlassy href="mailto:sandaru.suranjaya@icloud.com">
+                    Email
+                </SecondaryCtaHeroGlassy>
+                <SecondaryCtaHeroGlassy href="https://www.linkedin.com/in/sandaru-suranjaya/">
+                    LinkedIn
+                </SecondaryCtaHeroGlassy>
+                <SecondaryCtaHeroGlassy href="https://www.instagram.com/sandaru.me/">
+                    Instagram
+                </SecondaryCtaHeroGlassy>
+                <SecondaryCtaHeroGlassy href="https://x.com/sandaru_me">
+                    Twitter
+                </SecondaryCtaHeroGlassy>
             </div>
         </section>
     );
